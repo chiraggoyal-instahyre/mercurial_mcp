7d78f1e140536303db09ed7198bc38df3b99f6de
Chirag goyal <chirag.goyal@instahyre.com>
2025-11-15 19:18:14 +0530

    Remove commit cleaning command

--- helpers.py
+++ helpers.py
@@ -1,46 +1,8 @@
 import subprocess
 
-clean_diff_command = """
-  perl -0777 -pe '
-    # Remove everything from "Test Plan:" till commit end
-    s/Test Plan:.*?---END OF COMMIT---/---END OF COMMIT---/isg;
-
-    # Split into lines, filter out specific unwanted lines, then reassemble
-    @lines = grep {
-      !/Summary:/ &&         # Remove lines containing "Summary:"
-      !/Fixes T/ &&         # Remove lines containing "Fixes T"
-      !/Part of T/ &&         # Remove lines containing "Part of T"
-      !/\*\*Land on/ &&     # Remove lines containing "**Land on"
-      !/Caused by:/ &&        # Remove lines containing "Caused by:"
-      !/Reviewers:/ &&        # Remove lines containing "Reviewers:"
-      !/Reviewed By:/ &&        # Remove lines containing "Reviewed By:"
-      !/Maniphest Tasks:/ &&        # Remove lines containing "Maniphest Tasks:"
-      !/Differential Revision:/ &&        # Remove lines containing "Differential Revision:"
-      !/Reason for not mentioning/     # Remove lines containing "Reason for not mentioning"
-    } split /\n/;
-
-    $_ = join("\n", @lines) . "\n";
-
-    # Remove any files, revisions, or tasks
-    s/{F\d+}//g;
-    s/D\d+//g;
-    s/T\d+//g;
-
-    # Remove formatting
-    s/\*\*//g;  # Remove bold formatting
-    s/\/\///g;  # Remove italic formatting
-
-    # Remove attached URLs (keeping the name)
-    s/\[\[\s*[^|\]]+\|\s*([^\]]+)\s*\]\]/$1/g;
-
-    # Normailize line breaks
-    s/\n{2,}/\n\n/g;  # Replace three or more newlines with two newlines
-  '
-"""
-
 def get_commit_desc(commit_hash: str, CWD: str) -> str:
     try:
-        command = f"hg log -r {commit_hash} --template '{{desc}}' | {clean_diff_command}"
+        command = f"hg log -r {commit_hash} --template '{{desc}}'"
         return subprocess.run(
             command, 
             shell=True, 
